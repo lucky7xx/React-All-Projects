c421254052c86f017c2f4c990f86bf9243e7adad
Lucky Patel <lbp7198@gmail.com>
2022-12-06 14:38:05 +0530

    5th commit

--- src/WeatherApp/WeatherCard.js
+++ src/WeatherApp/WeatherCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios, { Axios } from "axios";
 import {
   ContainerDiv,
   InnerDiv,
@@ -7,13 +8,14 @@ import {
 } from "./StyledComponents";
 function WeatherCard() {
   const [input, SetInput] = useState("");
-
   const [apiData, setApiData] = useState([]);
   const [temp, setTemp] = useState([]);
   const [minTemp, setMinTemp] = useState([]);
   const [maxTemp, setMaxTemp] = useState([]);
   const [pressure, setPressure] = useState([]);
+  const [humidity, setHumidity] = useState([]);
   const [error, setError] = useState([]);
+  const [wind, setWind] = useState([]);
 
   const handleOnChange = (e) => {
     SetInput(e.target.value);
@@ -21,19 +23,30 @@ function WeatherCard() {
   };
 
   const handleButtonClick = () => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=0f5ee70dcdfa521f9fbd0cacb97d71b7`
-    )
-      .then((response) => response.json())
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=0f5ee70dcdfa521f9fbd0cacb97d71b7`
+      )
+
       .then((data) => {
-        setApiData(data.weather);
-        setTemp(Math.round(data.main.temp - 273));
-        setMinTemp(Math.round(data.main.temp_min - 273));
-        setMaxTemp(Math.round(data.main.temp_max - 273));
-        setPressure(data.main.pressure);
+        setApiData(data.data.weather);
+        setTemp(Math.round(data.data.main.temp - 273));
+        setMinTemp(Math.round(data.data.main.temp_min - 273));
+        setMaxTemp(Math.round(data.data.main.temp_max - 273));
+        setPressure(data.data.main.pressure);
+        setHumidity(data.data.main.humidity);
+        setWind(data.data.wind.speed);
+        setError("");
       })
-      .catch((error) => {
-        setError(error.cod);
+      .catch((e) => {
+        setError(`${input} not found.`);
+        setApiData([]);
+        setTemp("");
+        setMinTemp("");
+        setMaxTemp("");
+        setPressure("");
+        setHumidity("");
+        setWind("");
       });
   };
 
@@ -63,7 +76,9 @@ function WeatherCard() {
           <p>Minimum Temperature: {minTemp}°C</p>
           <p>Maximum Temperature: {maxTemp}°C</p>
           <p>Pressure: {pressure}pa</p>
-          <p>{error}</p>
+          <p>Humidity:{humidity}%</p>
+          <p>Wind Speed: {wind}</p>
+          <p>Error: {error}</p>
         </div>
       </ContainerDiv>
     </>
