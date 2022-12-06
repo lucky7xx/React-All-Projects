c7654eb54582d1c41601c2493dccdddc9acb70a9
Lucky Patel <lbp7198@gmail.com>
2022-12-06 14:38:38 +0530

    5th commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import WeatherApp from "./WeatherApp/WeatherApp";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <BatchUsage /> */}
     <WeatherApp />
   </React.StrictMode>
 );
